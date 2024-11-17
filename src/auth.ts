d6ca46f35c44210580cb268e94e9437c23dfcb3a
Alexandre Trotel <trotelalexandre@proton.me>
2024-11-17 18:56:08 +0100

    add: credentials auth

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,8 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -9,6 +11,43 @@ export const authOptions: NextAuthOptions = {
     GitHub({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "you@example.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (credentials?.password && user?.passwordHash) {
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.passwordHash
+          );
+
+          if (isValid) {
+            return user;
+          }
+        }
+
+        // register the user if not found
+        const newUser = await prisma.user.create({
+          data: {
+            email: credentials?.email,
+            passwordHash: await bcrypt.hash(credentials?.password, 10),
+          },
+        });
+
+        return newUser;
+      },
     }),
   ],
 };
