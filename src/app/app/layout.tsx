73666cea0a540dd57a07a73430c49ba8831a8184
Alexandre Trotel <trotelalexandre@proton.me>
2024-11-17 13:17:31 +0100

    add: sidebar

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,22 +1,52 @@
-import Header from "@/components/core/header";
 import React from "react";
+import { AppSidebar } from "@/components/core/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { cookies } from "next/headers";
+import { Separator } from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
 
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
-export default function AppLayout({ children }: AppLayoutProps) {
+export default async function AppLayout({ children }: AppLayoutProps) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar:state")?.value === "true";
+
+  const date = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-
-      <main className="flex-grow container mx-auto px-4 py-8">{children}</main>
-
-      <footer className="border-t">
-        <div className="container mx-auto px-4 py-4 text-center text-sm text-muted-foreground">
-          © 2024 Alexandre Trotel. All rights reserved.
-        </div>
-      </footer>
-    </div>
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4 justify-between">
+          <div className="flex items-center gap-2">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <span className="font-semibold">Welcome, Guest 👋</span>
+          </div>
+          <span className="text-muted-foreground text-sm hidden md:block">
+            {date}
+          </span>
+        </header>
+        <main className="flex-grow container mx-auto px-4 py-8">
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
--- src/components/core/dark-mode-toggle.tsx
+++ src/components/core/dark-mode-toggle.tsx
@@ -3,8 +3,8 @@
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { Button } from "@/components/ui/button";
 import { useCallback } from "react";
+import { SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
 
 export function DarkModeToggle() {
   const { theme, setTheme } = useTheme();
@@ -14,10 +14,13 @@ export function DarkModeToggle() {
   }, [theme, setTheme]);
 
   return (
-    <Button variant="ghost" size="icon" onClick={handleToggle}>
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+    <SidebarMenuItem className="list-none">
+      <SidebarMenuButton onClick={handleToggle}>
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <span>{theme === "light" ? "Light" : "Dark"} mode</span>
+        <span className="sr-only">Toggle theme</span>
+      </SidebarMenuButton>
+    </SidebarMenuItem>
   );
 }
