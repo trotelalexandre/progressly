95a5992ed0bfe175dbd8c36780731949ccd0074e
Alexandre Trotel <trotelalexandre@proton.me>
2024-11-18 17:50:56 +0100

    fix: schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -13,13 +13,15 @@ import {
 import { sql } from "drizzle-orm";
 
 export const users = pgTable("users", {
-  id: uuid("id").primaryKey(),
+  id: uuid("id")
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
   email: text("email").notNull().unique(),
   passwordHash: text("password_hash"),
-  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
-  updatedAt: timestamp("updated_at", { withTimezone: true }).default(
-    sql`CURRENT_TIMESTAMP`
-  ),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
   emailVerified: timestamp("email_verified", { withTimezone: true }),
   image: text("image"),
   name: text("name"),
@@ -42,10 +44,10 @@ export const accounts = pgTable(
     scope: text("scope"),
     idToken: text("id_token"),
     sessionState: text("session_state"),
-    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).default(
-      sql`CURRENT_TIMESTAMP`
-    ),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
   },
   (table) => ({
     uniqueProvider: uniqueIndex("unique_provider").on(
@@ -65,10 +67,10 @@ export const sessions = pgTable(
       .notNull()
       .references(() => users.id, { onDelete: "cascade" }),
     expires: timestamp("expires", { withTimezone: true }).notNull(),
-    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).default(
-      sql`CURRENT_TIMESTAMP`
-    ),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
   },
   (table) => ({
     userIndex: index("sessions_user_index").on(table.userId),
@@ -82,6 +84,9 @@ export const verificationTokens = pgTable(
     identifier: text("identifier").notNull(),
     token: text("token").notNull(),
     expires: timestamp("expires", { withTimezone: true }).notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
   },
   (table) => ({
     uniqueIdentifier: uniqueIndex("verification_tokens_unique_identifier").on(
@@ -105,6 +110,9 @@ export const authenticators = pgTable(
     credentialDeviceType: text("credential_device_type").notNull(),
     credentialBackedUp: boolean("credential_backed_up").notNull(),
     transports: text("transports"),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
   },
   (table) => ({
     uniqueCredential: uniqueIndex("authenticators_unique_credential").on(
@@ -124,7 +132,9 @@ export const habits = pgTable(
       .references(() => users.id, { onDelete: "cascade" }),
     name: text("name").notNull(),
     completedDays: integer("completed_days[]").array(),
-    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
   },
   (table) => ({
     userIndex: index("habits_user_index").on(table.userId),
@@ -146,6 +156,9 @@ export const transactions = pgTable(
     date: timestamp("date", { withTimezone: true }).defaultNow(),
     type: text("type").notNull(),
     investmentId: integer("investment_id").references(() => investments.id),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
   },
   (table) => ({
     userCategoryDateIndex: index("transactions_user_category_date_index").on(
@@ -169,6 +182,9 @@ export const budgets = pgTable(
     expenses: numeric("expenses").notNull(),
     investments: numeric("investments").notNull(),
     savings: numeric("savings").notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
   },
   (table) => ({
     userYearMonthIndex: index("budgets_user_year_month_index").on(
@@ -190,6 +206,9 @@ export const investments = pgTable(
     category: text("category").notNull(),
     buyPrice: numeric("buy_price").notNull(),
     quantity: integer("quantity").notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
   },
   (table) => ({
     userCategoryIndex: index("investments_user_category_index").on(
@@ -210,6 +229,9 @@ export const readings = pgTable(
     totalPages: integer("total_pages").notNull(),
     currentPage: integer("current_page").notNull(),
     isCompleted: boolean("is_completed").default(false),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
   },
   (table) => ({
     userCompletionIndex: index("readings_user_completion_index").on(
