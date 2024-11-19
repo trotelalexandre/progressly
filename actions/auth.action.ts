a9d7586e381c76adf04059a2cca092e092daafad
Alexandre Trotel <trotelalexandre@proton.me>
2024-11-19 21:07:49 +0100

    fix: process env

--- actions/auth.action.ts
+++ actions/auth.action.ts
@@ -4,17 +4,17 @@ import "server-only";
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 
-const PROGRESSLY_ENV = process.env.PROGRESSLY_ENV;
-
-const redirectTo =
-  PROGRESSLY_ENV === "PRODUCTION"
-    ? "https://progressly-prod.vercel.app/auth/callback"
-    : PROGRESSLY_ENV === "PREVIEW"
-      ? "https://progressly-staging.vercel.app/auth/callback"
-      : "http://localhost:3000/auth/callback";
-
 export const signInWithGoogle = async () => {
   const supabase = await createClient();
+
+  const PROGRESSLY_ENV = process.env.PROGRESSLY_ENV;
+
+  const redirectTo =
+    PROGRESSLY_ENV === "PRODUCTION"
+      ? "https://progressly-prod.vercel.app/auth/callback"
+      : PROGRESSLY_ENV === "PREVIEW"
+        ? "https://progressly-staging.vercel.app/auth/callback"
+        : "http://localhost:3000/auth/callback";
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
