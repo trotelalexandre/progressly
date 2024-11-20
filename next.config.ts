5f467a423679c1edb72570e29e821ec4064adbdd
Alexandre Trotel <trotelalexandre@proton.me>
2024-11-20 17:18:46 +0100

    fix: pwa

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import withPWA from "next-pwa";
 
 const config = withPWA({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/app",
+  sw: "service-worker.js",
 });
 
 const nextConfig: NextConfig = {
