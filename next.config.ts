8b6ef17469bd402a7f4f8937e3f068b5f0ebc885
Alexandre Trotel <trotelalexandre@proton.me>
2024-11-20 17:17:56 +0100

    add: pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
+
+const config = withPWA({
+  dest: "public",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  ...config,
 };
 
 export default nextConfig;
