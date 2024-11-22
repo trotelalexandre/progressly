a181cf73e10cc8d2560cb3ce1e5ecfde7954c9ec
Alexandre Trotel <trotelalexandre@proton.me>
2024-11-22 13:01:18 +0100

    fix: categories

--- src/utils/db/budgets/categories.ts
+++ src/utils/db/budgets/categories.ts
@@ -1,7 +1,10 @@
 import { db } from "@/db/db";
+import { asc } from "drizzle-orm";
+import { transaction_categories } from "@/db/schema/budgets";
 
 export const fetchCategories = db.query.transaction_categories
   .findMany({
     columns: { name: true, type: true },
+    orderBy: [asc(transaction_categories.name)],
   })
   .prepare("fetch_categories");
