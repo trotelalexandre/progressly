101dbb60ab71f606af6041e2cf88e23df6246190
Alexandre Trotel <trotelalexandre@proton.me>
2024-12-07 13:07:48 +0100

    fix: progress bar

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -5,24 +5,39 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
 
+interface ProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  value: number;
+  overLimitClassName?: string;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
-      className,
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-));
+>(
+  (
+    { className, value, overLimitClassName = "bg-destructive", ...props },
+    ref
+  ) => (
+    <ProgressPrimitive.Root
+      ref={ref}
+      className={cn(
+        "relative h-2 w-full overflow-hidden rounded-full",
+        value >= 100 ? overLimitClassName : "bg-primary/20",
+        className
+      )}
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        className={cn(
+          "h-full w-full flex-1 transition-all",
+          value >= 100 ? overLimitClassName : "bg-primary"
+        )}
+        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      />
+    </ProgressPrimitive.Root>
+  )
+);
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
 export { Progress };
